3435f12d94e90c23f67d28fe7704977d441aed70
Leonardo Bispo <34199302+ldab@users.noreply.github.com>
2022-03-17 10:37:22 +0100

    Implement CI on GitHub Actions (#6)
    * add secrets.h
    * keep *.elf for debugging

---
+++ include/secrets.h
@@ -0,0 +1,18 @@
+const char* s_wifi_ssid = "SSID";
+const char* s_wifi_password = "PASSWORD";
+
+const char* s_mqtt_server = "mqtt.flespi.io";
+const char* s_mqtt_user =
+    "igjoijsfiodsjfoijsdoifjsdiofjsdi";
+const char* s_mqtt_pass = "";
+uint16_t s_mqtt_port = 1883;
+
+#define BLYNK_AUTH_TOKEN            "dsfsdfsdfdsf"
+#define BLYNK_DEVICE_NAME           "MyKisdfsdfln"
+
+// #define BLYNK_AUTH_TOKEN            "sdfsdf"
+
+char s_blynk_auth[] = BLYNK_AUTH_TOKEN;
+
+#define PAPERTRAIL_HOST "logs2.papertrailapp.com"
+#define PAPERTRAIL_PORT 65464684
